8163afbeddac01fd3a51c38a9961bb93b7876c38
CK <designedbykang@gmail.com>
2026-07-04 17:22:42 +0100

    Delete src directory

--- src/lib/supabase.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-// These variables are automatically injected by Vercel during the build.
-// You do not need to add them to a .env file for production to work.
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-
-// Create a single instance of the Supabase client to be used throughout the app
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
