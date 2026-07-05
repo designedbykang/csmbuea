c6a93c02c20cd145d8d354dcef41804e8b322046
CK <designedbykang@gmail.com>
2026-07-05 05:27:36 +0100

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,23 @@
-import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 
 export async function middleware(req: NextRequest) {
-  const res = NextResponse.next();
-  const supabase = createMiddlewareClient({ req, res });
-  const { data: { session } } = await supabase.auth.getSession();
+  const res = NextResponse.next()
+  const supabase = createMiddlewareClient({ req, res })
 
-  // If user is not logged in and tries to access /admin/*, redirect to /admin/login
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
+  // If user is not logged in and tries to access /admin, redirect to /admin/login
   if (!session && req.nextUrl.pathname.startsWith('/admin')) {
-    const redirectUrl = new URL('/admin/login', req.url);
-    return NextResponse.redirect(redirectUrl);
+    const redirectUrl = new URL('/admin/login', req.url)
+    return NextResponse.redirect(redirectUrl)
   }
-  return res;
+  return res
 }
 
 export const config = {
   matcher: ['/admin/:path*'],
-};
+}
